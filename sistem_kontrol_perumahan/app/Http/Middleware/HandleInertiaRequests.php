0a1d2fdc2b4f83058fdcb6b281e78c68451b5159
darunnadwah <daruncraftyt3011@gmail.com>
2026-07-09 15:59:26 +0700

    sidebar

--- sistem_kontrol_perumahan/app/Http/Middleware/HandleInertiaRequests.php
+++ sistem_kontrol_perumahan/app/Http/Middleware/HandleInertiaRequests.php
@@ -28,12 +28,22 @@ class HandleInertiaRequests extends Middleware
      * @return array<string, mixed>
      */
     public function share(Request $request): array
-    {
-        return [
-            ...parent::share($request),
-            'auth' => [
-                'user' => $request->user(),
-            ],
-        ];
-    }
+        {
+            return [
+                ...parent::share($request),
+
+                'auth' => [
+                    'user' => $request->user()
+                        ? [
+                            'id' => $request->user()->id,
+                            'name' => $request->user()->name,
+                            'email' => $request->user()->email,
+
+                            // Role dari Spatie
+                            'roles' => $request->user()->getRoleNames()->toArray(),
+                        ]
+                        : null,
+                ],
+            ];
+        }
 }
